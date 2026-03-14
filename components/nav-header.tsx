b6be9eeaf6c727e4a3cbaae512b2d78c5e63de49
samrathreddy <samrathreddy04@gmail.com>
2026-03-15 01:00:06 +0530

    refactor: reorder social links (GitHub, LinkedIn, LeetCode) & clean up hover effects

--- components/nav-header.tsx
+++ components/nav-header.tsx
@@ -285,42 +285,39 @@ export function NavHeader() {
             {isDesktop && (
               <div className="flex items-center gap-2">
                 <div className="flex items-center gap-1">
-                  <Link 
-                    href="https://leetcode.com/u/samrathreddy/" 
+                  <Link
+                    href="https://github.com/samrathreddy"
                     target="_blank"
                     className="group relative p-2 transition-all duration-300"
                   >
-                    <div className="absolute -inset-2 bg-gradient-to-r from-[#FFA116] via-[#B3B3B3] to-[#FFA116] rounded-lg opacity-0 group-hover:opacity-75 transition-opacity duration-300 blur" />
-                    <div className="relative flex items-center text-[#989898] group-hover:text-[#FFA116] transition-colors">
-                      <svg 
-                        viewBox="0 0 24 24" 
-                        fill="currentColor" 
-                        className="w-5 h-5 transition-transform group-hover:scale-110"
-                      >
-                        <path d="M13.483 0a1.374 1.374 0 0 0-.961.438L7.116 6.226l-3.854 4.126a5.266 5.266 0 0 0-1.209 2.104 5.35 5.35 0 0 0-.125.513 5.527 5.527 0 0 0 .062 2.362 5.83 5.83 0 0 0 .349 1.017 5.938 5.938 0 0 0 1.271 1.818l4.277 4.193.039.038c2.248 2.165 5.852 2.133 8.063-.074l2.396-2.392c.54-.54.54-1.414.003-1.955a1.378 1.378 0 0 0-1.951-.003l-2.396 2.392a3.021 3.021 0 0 1-4.205.038l-.02-.019-4.276-4.193c-.652-.64-.972-1.469-.948-2.263a2.68 2.68 0 0 1 .066-.523 2.545 2.545 0 0 1 .619-1.164L9.13 8.114c1.058-1.134 3.204-1.27 4.43-.278l3.501 2.831c.593.48 1.461.387 1.94-.207a1.384 1.384 0 0 0-.207-1.943l-3.5-2.831c-.8-.647-1.766-1.045-2.774-1.202l2.015-2.158A1.384 1.384 0 0 0 13.483 0zm-2.866 12.815a1.38 1.38 0 0 0-1.38 1.382 1.38 1.38 0 0 0 1.38 1.382H20.79a1.38 1.38 0 0 0 1.38-1.382 1.38 1.38 0 0 0-1.38-1.382z" />
-                      </svg>
-                    </div>
-                  </Link>
-
-                  <Link 
-                    href="https://github.com/samrathreddy" 
-                    target="_blank"
-                    className="group relative p-2 transition-all duration-300"
-                  >
-                    <div className="absolute -inset-2 bg-gradient-to-r from-[#6e5494] via-[#B3B3B3] to-[#6e5494] rounded-lg opacity-0 group-hover:opacity-75 transition-opacity duration-300 blur" />
                     <div className="relative flex items-center text-[#989898] group-hover:text-white transition-colors">
                       <Github className="w-5 h-5 transition-transform group-hover:scale-110" />
                     </div>
                   </Link>
 
-                  <Link 
-                    href="https://www.linkedin.com/in/samrath-reddy/" 
+                  <Link
+                    href="https://www.linkedin.com/in/samrath-reddy/"
                     target="_blank"
                     className="group relative p-2 transition-all duration-300"
                   >
-                    <div className="absolute -inset-2 bg-gradient-to-r from-[#0A66C2] via-[#B3B3B3] to-[#0A66C2] rounded-lg opacity-0 group-hover:opacity-75 transition-opacity duration-300 blur" />
                     <div className="relative flex items-center text-[#989898] group-hover:text-[#0A66C2] transition-colors">
                       <Linkedin className="w-5 h-5 transition-transform group-hover:scale-110" />
+                    </div>
+                  </Link>
+
+                  <Link
+                    href="https://leetcode.com/u/samrathreddy/"
+                    target="_blank"
+                    className="group relative p-2 transition-all duration-300"
+                  >
+                    <div className="relative flex items-center text-[#989898] group-hover:text-[#FFA116] transition-colors">
+                      <svg
+                        viewBox="0 0 24 24"
+                        fill="currentColor"
+                        className="w-5 h-5 transition-transform group-hover:scale-110"
+                      >
+                        <path d="M13.483 0a1.374 1.374 0 0 0-.961.438L7.116 6.226l-3.854 4.126a5.266 5.266 0 0 0-1.209 2.104 5.35 5.35 0 0 0-.125.513 5.527 5.527 0 0 0 .062 2.362 5.83 5.83 0 0 0 .349 1.017 5.938 5.938 0 0 0 1.271 1.818l4.277 4.193.039.038c2.248 2.165 5.852 2.133 8.063-.074l2.396-2.392c.54-.54.54-1.414.003-1.955a1.378 1.378 0 0 0-1.951-.003l-2.396 2.392a3.021 3.021 0 0 1-4.205.038l-.02-.019-4.276-4.193c-.652-.64-.972-1.469-.948-2.263a2.68 2.68 0 0 1 .066-.523 2.545 2.545 0 0 1 .619-1.164L9.13 8.114c1.058-1.134 3.204-1.27 4.43-.278l3.501 2.831c.593.48 1.461.387 1.94-.207a1.384 1.384 0 0 0-.207-1.943l-3.5-2.831c-.8-.647-1.766-1.045-2.774-1.202l2.015-2.158A1.384 1.384 0 0 0 13.483 0zm-2.866 12.815a1.38 1.38 0 0 0-1.38 1.382 1.38 1.38 0 0 0 1.38 1.382H20.79a1.38 1.38 0 0 0 1.38-1.382 1.38 1.38 0 0 0-1.38-1.382z" />
+                      </svg>
                     </div>
                   </Link>
                 </div>
@@ -453,31 +450,11 @@ export function NavHeader() {
                     "flex items-center gap-6",
                     isTablet ? "justify-center" : "justify-start"
                   )}>
-                    <Link 
-                      href="https://leetcode.com/u/samrathreddy/" 
+                    <Link
+                      href="https://github.com/samrathreddy"
                       target="_blank"
                       className="group relative"
                     >
-                      <div className="absolute -inset-2 bg-gradient-to-r from-[#FFA116] via-[#B3B3B3] to-[#FFA116] rounded-lg opacity-0 group-hover:opacity-75 transition-opacity duration-300 blur" />
-                      <div className="relative flex items-center text-[#989898] group-hover:text-[#FFA116] transition-colors">
-                        <svg 
-                          viewBox="0 0 24 24" 
-                          fill="currentColor" 
-                          className={cn(
-                            "transition-transform group-hover:scale-110",
-                            isTablet ? "w-6 h-6" : "w-5 h-5"
-                          )}
-                        >
-                          <path d="M13.483 0a1.374 1.374 0 0 0-.961.438L7.116 6.226l-3.854 4.126a5.266 5.266 0 0 0-1.209 2.104 5.35 5.35 0 0 0-.125.513 5.527 5.527 0 0 0 .062 2.362 5.83 5.83 0 0 0 .349 1.017 5.938 5.938 0 0 0 1.271 1.818l4.277 4.193.039.038c2.248 2.165 5.852 2.133 8.063-.074l2.396-2.392c.54-.54.54-1.414.003-1.955a1.378 1.378 0 0 0-1.951-.003l-2.396 2.392a3.021 3.021 0 0 1-4.205.038l-.02-.019-4.276-4.193c-.652-.64-.972-1.469-.948-2.263a2.68 2.68 0 0 1 .066-.523 2.545 2.545 0 0 1 .619-1.164L9.13 8.114c1.058-1.134 3.204-1.27 4.43-.278l3.501 2.831c.593.48 1.461.387 1.94-.207a1.384 1.384 0 0 0-.207-1.943l-3.5-2.831c-.8-.647-1.766-1.045-2.774-1.202l2.015-2.158A1.384 1.384 0 0 0 13.483 0zm-2.866 12.815a1.38 1.38 0 0 0-1.38 1.382 1.38 1.38 0 0 0 1.38 1.382H20.79a1.38 1.38 0 0 0 1.38-1.382 1.38 1.38 0 0 0-1.38-1.382z" />
-                        </svg>
-                      </div>
-                    </Link>
-                    <Link 
-                      href="https://github.com/samrathreddy" 
-                      target="_blank"
-                      className="group relative"
-                    >
-                      <div className="absolute -inset-2 bg-gradient-to-r from-[#6e5494] via-[#B3B3B3] to-[#6e5494] rounded-lg opacity-0 group-hover:opacity-75 transition-opacity duration-300 blur" />
                       <div className="relative flex items-center text-[#989898] group-hover:text-white transition-colors">
                         <Github className={cn(
                           "transition-transform group-hover:scale-110",
@@ -485,17 +462,34 @@ export function NavHeader() {
                         )} />
                       </div>
                     </Link>
-                    <Link 
-                      href="https://www.linkedin.com/in/samrath-reddy/" 
+                    <Link
+                      href="https://www.linkedin.com/in/samrath-reddy/"
                       target="_blank"
                       className="group relative"
                     >
-                      <div className="absolute -inset-2 bg-gradient-to-r from-[#0A66C2] via-[#B3B3B3] to-[#0A66C2] rounded-lg opacity-0 group-hover:opacity-75 transition-opacity duration-300 blur" />
                       <div className="relative flex items-center text-[#989898] group-hover:text-[#0A66C2] transition-colors">
                         <Linkedin className={cn(
                           "transition-transform group-hover:scale-110",
                           isTablet ? "w-6 h-6" : "w-5 h-5"
                         )} />
+                      </div>
+                    </Link>
+                    <Link
+                      href="https://leetcode.com/u/samrathreddy/"
+                      target="_blank"
+                      className="group relative"
+                    >
+                      <div className="relative flex items-center text-[#989898] group-hover:text-[#FFA116] transition-colors">
+                        <svg
+                          viewBox="0 0 24 24"
+                          fill="currentColor"
+                          className={cn(
+                            "transition-transform group-hover:scale-110",
+                            isTablet ? "w-6 h-6" : "w-5 h-5"
+                          )}
+                        >
+                          <path d="M13.483 0a1.374 1.374 0 0 0-.961.438L7.116 6.226l-3.854 4.126a5.266 5.266 0 0 0-1.209 2.104 5.35 5.35 0 0 0-.125.513 5.527 5.527 0 0 0 .062 2.362 5.83 5.83 0 0 0 .349 1.017 5.938 5.938 0 0 0 1.271 1.818l4.277 4.193.039.038c2.248 2.165 5.852 2.133 8.063-.074l2.396-2.392c.54-.54.54-1.414.003-1.955a1.378 1.378 0 0 0-1.951-.003l-2.396 2.392a3.021 3.021 0 0 1-4.205.038l-.02-.019-4.276-4.193c-.652-.64-.972-1.469-.948-2.263a2.68 2.68 0 0 1 .066-.523 2.545 2.545 0 0 1 .619-1.164L9.13 8.114c1.058-1.134 3.204-1.27 4.43-.278l3.501 2.831c.593.48 1.461.387 1.94-.207a1.384 1.384 0 0 0-.207-1.943l-3.5-2.831c-.8-.647-1.766-1.045-2.774-1.202l2.015-2.158A1.384 1.384 0 0 0 13.483 0zm-2.866 12.815a1.38 1.38 0 0 0-1.38 1.382 1.38 1.38 0 0 0 1.38 1.382H20.79a1.38 1.38 0 0 0 1.38-1.382 1.38 1.38 0 0 0-1.38-1.382z" />
+                        </svg>
                       </div>
                     </Link>
                   </div>
